aafb205403c2b41835598f64ea7c5ea64906d28f
eduardomzm <eduardo.mezam@outlook.com>
2025-06-26 10:59:00 -0600

    Changes to Clase4.java

--- paquete1/Clase4.java
+++ paquete1/Clase4.java
@@ -1,18 +1,27 @@
-package paquete1
+package paquete1;
 
 public class Clase4 {
     private boolean x;
+    private int y;
 
-    public boolean isX() {
+    public boolean getX() {
         return x;
     }
 
     public void setX(boolean x) {
         this.x = x;
     }
+    
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 
     @Override
     public String toString() {
-        return "Clase4 [x=" + x + "]";
-    }    
+        return "Clase4 [x=" + x + ", y=" + y + "]";
+    } 
 }
